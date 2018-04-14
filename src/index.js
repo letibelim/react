61c250a728931411f81d992140e1a635233f3a81
Thibault Le Grand <thibault.legrand@gmail.com>
2018-04-14 08:25:03 +0200

    step 14/04/2018

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-// Ici on code notre App
+import React from 'react';
+import { render } from 'react-dom';
+// CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+// JS PERSO
+import { sampleText } from './sampleText';
 
-console.log("welcome to react");
+class App extends React.Component {
+
+	state = {
+		text : sampleText
+	};
+
+
+	editText = (event) => {
+		const text = event.target.value;
+		this.setState({ text })
+
+	};
+
+
+	render(){
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-6">
+						<textarea
+						value={this.state.text} 
+						name="" id="" 
+						cols="30" 
+						rows="35" 
+						className="form-control"
+						onChange={(e) =>  this.editText(e)}
+						>
+						
+						</textarea>
+					</div>
+					<div className="col-sm-6">
+						{ this.state.text }
+					</div>
+
+				</div>
+			</div>
+			)
+	}
+}
+
+render (
+	<App />, document.getElementById('root')
+	);
