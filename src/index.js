a08f595efbd9b2e9b63f87dba3f843a0dc6aea04
Thibault Le Grand <thibault.legrand@gmail.com>
2018-04-14 15:50:46 +0200

    markdown finished

--- src/index.js
+++ src/index.js
@@ -5,25 +5,48 @@ import './style/css/bootstrap.min.css';
 import './index.css';
 // JS PERSO
 import { sampleText } from './sampleText';
+// MARKED.JS
+import marked from 'marked';
+
 
 class App extends React.Component {
 
+	// STATE
 	state = {
 		text : sampleText
 	};
 
+	// LifeCycleCAllbacks
 
+	componentWillMount(){
+		const text = localStorage.getItem('text');
+		if (text){
+		    this.setState({ text });
+		}
+	}
+
+
+	componentWillUpdate(nextProps, nexState){
+		localStorage.setItem('text', nexState.text);
+	};
+
+	// Methods
 	editText = (event) => {
 		const text = event.target.value;
 		this.setState({ text })
 
 	};
 
+	renderText = (text) => {
+		const renderText = marked(text, {sanitize:true});
+		return { __html: renderText };
+	};
 
+	// Render
 	render(){
 		return (
 			<div className="container">
-				<div className="row"> test
+				<div className="row">
 					<div className="col-sm-6">
 						<textarea
 						value={this.state.text} 
@@ -37,7 +60,7 @@ class App extends React.Component {
 						</textarea>
 					</div>
 					<div className="col-sm-6">
-						{ this.state.text }
+						<div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
 					</div>
 
 				</div>
